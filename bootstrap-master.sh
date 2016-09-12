be0f547acbf904acb2783f306c09c20a7ddc7e76
Thomas Bianchi <thomas.bianchi8@gmail.com>
2016-09-12 11:49:42 +0200

    inventory created by master provisoner

--- bootstrap-master.sh
+++ bootstrap-master.sh
@@ -10,3 +10,19 @@ install -g vagrant -o vagrant -m0644 /dev/null  /home/vagrant/.ssh/known_hosts
 ssh-keyscan hadoop-master >> /home/vagrant/.ssh/known_hosts
 # Authorize ssh myself
 cat /home/vagrant/.ssh/id_rsa.pub >> /home/vagrant/.ssh/authorized_keys
+
+cat > /vagrant/inventory.ini << EOF
+[master]
+hadoop-master
+
+[slave]
+EOF
+
+for f in $(sed -n '/vagrant-hostmanager-start/ {
+         :next
+         n; /vagrant-hostmanager-end/q; p; b next
+       }' /etc/hosts | grep -v $(hostname) | awk '{print $2}') ; do
+         if [[ $f == hadoop-slave* ]]; then
+           echo "$f" >> /vagrant/inventory.ini
+         fi
+done
